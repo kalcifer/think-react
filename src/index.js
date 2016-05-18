24d642668c3874468c087c6950d04f7033fd4e7c
Rahul Chanila <rahul@unbxd.com>
2016-05-18 12:03:29 +0530

    add products json array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
 ReactDOM.render(
     <h1>Hello, world!</h1>,
     document.getElementById('example')
